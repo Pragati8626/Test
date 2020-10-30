9a9a84991d84f2c5d6e4e3de6e23b51f9d5fb7f1
pragati1234 <pragati@clinivault.org>
2020-10-30 14:49:09 +0530

    first commit

---
+++ pandas_assg.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+download_url = ("https://raw.githubusercontent.com/fivethirtyeight/data/master/college-majors/recent-grads.csv")
+
+df = pd.read_csv(download_url)
+
+print(df.head())
+
+rcParams["figure.figsize"]= 10,10
+
+df.plot(x="Rank", y=["P25th", "P75th"], kind="line")
+
+plt.grid(True, color="k", linestyle=":")
+
+plt.show()
